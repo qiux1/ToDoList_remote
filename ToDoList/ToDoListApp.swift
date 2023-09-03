82a6d73693485e600e08f9c63e095dcfd5776e3a
xinyuqiu23 <xqiu@decisionlinks.com>
2023-09-03 19:03:01 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Xinyu Qiu on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
