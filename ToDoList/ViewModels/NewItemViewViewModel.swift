d444e3a19b22da4c71e49df4a04ac4ac00600a0c
xinyuqiu23 <xqiu@decisionlinks.com>
2023-09-04 15:48:22 -0400

    implement save function for new item so that it can be saved in Firestone

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Xinyu Qiu on 9/3/23.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -16,7 +17,30 @@ class NewItemViewViewModel: ObservableObject {
     }
     
     func save(){
+        guard canSave else {
+            return
+        }
+        //get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
         
+        //create a model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        
+        //save the model to database as subcollection of the user
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)//model ID
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
